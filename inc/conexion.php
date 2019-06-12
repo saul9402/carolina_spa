6e1b8692d8b64b27e948b8644a4e5de4b1f04a1a
Saul Avila <saul_harry_@hotmail.com>
2019-06-12 13:51:17 -0500

    se agrega conexion a base de datos con php

---
+++ inc/conexion.php
@@ -0,0 +1,13 @@
+<?php
+try{
+    $bd = new mysqli('localhost:3307', 'root', '', 'carolina');
+} catch(Exception $e){
+    echo $e->getMessage();
+    exit;
+}
+/*
+if($bd->ping()){
+    echo 'Todo bien';
+}else{
+    echo $bd->connect_error;
+}*/
